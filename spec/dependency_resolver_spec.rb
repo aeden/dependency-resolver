835c8a312ab57986883fc67360e4ea1f8901dff2
Anthony Eden <anthonyeden@gmail.com>
2011-02-15 14:27:57 -0500

    correct name and clean up

---
+++ spec/dependency_resolver_spec.rb
@@ -0,0 +1,13 @@
+require 'dependency_resolver'
+
+describe DependencyResolver do
+  let(:resolver) { DependencyResolver.new('index.dnsimple.org') }
+  it "resolves dependencies" do
+    the_gem = resolver.dependencies('example', '1.0.0')
+    the_gem.children.map(&:fqn).sort.should eq([
+      'another-0.9.9', 
+      'dependency-2.1.0', 
+      'yet-another-3.4.0'
+    ])
+  end
+end
